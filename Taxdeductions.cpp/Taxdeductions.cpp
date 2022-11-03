a38feeefb64765ec97ac5a071ce5808793ca8d71
RoyKyalo <114662432+RoyKyalo@users.noreply.github.com>
2022-11-03 19:55:28 +0300

    Add files via upload

---
+++ Taxdeductions.cpp/Taxdeductions.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main()
+ float PAYE, Gross, Net;
+
+ cout<< "Insert Gross";
+ cin >> Gross;
+ PAYE = 25% * 10,000;
+ Net = 10,000 - PAYE;
+
+ cout<<  "Net =" <<Net;
+
+return 0
